c6d51ce46c34d6bd2b57713340d917a3b8116026
Henrique <henriquesoftware@outlook.com>
2017-08-31 10:27:08 -0300

    test

--- src/main/java/br/com/contability/configuracao/AgendadorTarefas.java
+++ src/main/java/br/com/contability/configuracao/AgendadorTarefas.java
@@ -1,5 +1,6 @@
 package br.com.contability.configuracao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,13 @@ import br.com.contability.business.services.UsuarioServices;
 @Component
 public class AgendadorTarefas {
 
+	@Autowired
+	private UsuarioServices usuarioServices;
+
 	@Scheduled(fixedDelay = 900000)
 	public void ativaServidor() {
 
-		new UsuarioServices().getJpa().findAll();
+		usuarioServices.getJpa().findAll();
 
 	}
 
